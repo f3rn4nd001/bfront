747d0203207b690e83092d6c73adf39e1b9005c7
f3rn4nd001 <f3rn4nd0.v3ntur4@gmail.com>
2022-12-02 13:04:31 -0600

    detalle de direccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-//    direcurl:'http://localhost:8000/',
+    //direcurl:'http://localhost:8000/',
     direcurl:'https://bsl-full.com.mx/',
     apiHost:'http://bsl-full.com.mx/',
     apiServer:'http://app.bsl-full.com.mx/'
